e7cf0e73260e67aa632e9dba3f55d2c0bca5180d
SpencerF718 <spencer.flaharty@gmail.com>
2025-06-29 23:43:02 -0400

    Add fluid simulation struct
    Introduces a struct to represent the fluid and a couple of functions to initialize and free the Fluid struct.

---
+++ eulerian_fluid_sim/include/fluid_logic.h
@@ -0,0 +1,38 @@
+#ifndef FLUID_LOGIC_H
+#define FLUID_LOGIC_H
+
+#include <stddef.h>
+
+/**
+ * struct that holds grid-based data for Eulerian fluid simulation.
+ */
+typedef struct {
+    
+    int numCellsX;
+    int numCellsY;
+    size_t totalNumCells;
+    float cellSize;
+    float density;
+
+    float* velocityX;
+    float* velocityY;
+    float* newVelocityX;
+    float* newVelocityY;
+    float* pressure;
+    float* smokeDensity;
+    float* newSmokeDensity;
+    float* solidFlags;
+
+} Fluid;
+
+/**
+ * Initializes a new Fluid simulation.
+ */
+Fluid* fluid_init(float density, int numX, int numY, float cellSize);
+
+/**
+ * Frees memory for Fluid struct
+ */
+void fluid_free(Fluid* fluid_ptr);
+
+#endif 
