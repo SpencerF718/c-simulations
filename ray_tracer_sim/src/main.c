59baa69bca5df9b9c6772c6f71d093b5e9c3e302
SpencerF718 <spencer.flaharty@gmail.com>
2025-07-13 23:24:53 -0400

    Move rendering loop into main event loop
    Moved the rendering loop into the main loop for real time rendering.

--- ray_tracer_sim/src/main.c
+++ ray_tracer_sim/src/main.c
@@ -80,55 +80,6 @@ int main(int argc, char* argv[]) {
     SDL_SetRenderDrawColor(sdlRenderer, 0x00, 0x00, 0x00, 0xFF);
     SDL_RenderClear(sdlRenderer);
 
-    for (int y = 0; y < WINDOW_HEIGHT; y++) {
-        for (int x = 0; x < WINDOW_WIDTH; x++) {
-
-            double uNorm = (double)x / (WINDOW_WIDTH - 1.0) * 2.0 - 1.0;
-            double vNorm = (double)y / (WINDOW_HEIGHT - 1.0) * 2.0 - 1.0;
-
-            Vec3 rayDirection = {0};
-            rayDirection = vec3_add(rayDirection, vec3_scale(cameraRight, uNorm * halfWidth));
-            rayDirection = vec3_add(rayDirection, vec3_scale(cameraUp, vNorm * halfHeight));
-            rayDirection = vec3_sub(rayDirection, cameraForward);
-
-            rayDirection = vec3_normalize(rayDirection);
-            Ray primaryRay = {sceneCamera.position, rayDirection};
-
-            Color pixelColor = {0.0, 0.0, 0.0};
-            double closestIntersectionDistance = INFINITY;
-            Sphere* hitSphere = NULL;
-
-            for (int i = 0; i < numSpheres; i++) {
-                double currentIntersectionDistance;
-                if (ray_intersect_sphere(primaryRay, sceneSpheres[i], &currentIntersectionDistance)) {
-                    if (currentIntersectionDistance < closestIntersectionDistance) {
-                        closestIntersectionDistance = currentIntersectionDistance;
-                        hitSphere = &sceneSpheres[i];
-                    }
-                }
-            }
-
-            if (hitSphere != NULL) {
-
-                Vec3 intersectionPoint = vec3_add(primaryRay.origin, vec3_scale(primaryRay.direction, closestIntersectionDistance));
-                Vec3 surfaceNormal = vec3_normalize(vec3_sub(intersectionPoint, hitSphere->center));
-                Vec3 lightDirection = vec3_normalize(vec3_sub(lightPosition, intersectionPoint));
-                double diffuseFactor = fmax(0.0, vec3_dot(surfaceNormal, lightDirection));
-
-                pixelColor.x = hitSphere->color.x * lightColor.x * diffuseFactor + ambientLight.x;
-                pixelColor.y = hitSphere->color.y * lightColor.y * diffuseFactor + ambientLight.y;
-                pixelColor.z = hitSphere->color.z * lightColor.z * diffuseFactor + ambientLight.z;
-            }
-
-            Uint8 r = (Uint8)(fmax(0.0, fmin(1.0, pixelColor.x)) * 255);
-            Uint8 g = (Uint8)(fmax(0.0, fmin(1.0, pixelColor.y)) * 255);
-            Uint8 b = (Uint8)(fmax(0.0, fmin(1.0, pixelColor.z)) * 255);
-
-            SDL_SetRenderDrawColor(sdlRenderer, r, g, b, 0XFF);
-            SDL_RenderDrawPoint(sdlRenderer, x, y);
-        }
-    }
-
     int quitApplication = 0;
     SDL_Event eventHandler;
 
@@ -137,6 +88,57 @@ int main(int argc, char* argv[]) {
         while (SDL_PollEvent(&eventHandler) != 0) {
             if (eventHandler.type == SDL_QUIT) {
                 quitApplication = 1;
+            }
+        }
+        SDL_SetRenderDrawColor(sdlRenderer, 0x00, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(sdlRenderer);
+
+        for (int y = 0; y < WINDOW_HEIGHT; y++) {
+            for (int x = 0; x < WINDOW_WIDTH; x++) {
+
+                double uNorm = (double)x / (WINDOW_WIDTH - 1.0) * 2.0 - 1.0;
+                double vNorm = (double)y / (WINDOW_HEIGHT - 1.0) * 2.0 - 1.0;
+
+                Vec3 rayDirection = {0};
+                rayDirection = vec3_add(rayDirection, vec3_scale(cameraRight, uNorm * halfWidth));
+                rayDirection = vec3_add(rayDirection, vec3_scale(cameraUp, vNorm * halfHeight));
+                rayDirection = vec3_sub(rayDirection, cameraForward);
+
+                rayDirection = vec3_normalize(rayDirection);
+                Ray primaryRay = {sceneCamera.position, rayDirection};
+
+                Color pixelColor = {0.0, 0.0, 0.0};
+                double closestIntersectionDistance = INFINITY;
+                Sphere* hitSphere = NULL;
+
+                for (int i = 0; i < numSpheres; i++) {
+                    double currentIntersectionDistance;
+                    if (ray_intersect_sphere(primaryRay, sceneSpheres[i], &currentIntersectionDistance)) {
+                        if (currentIntersectionDistance < closestIntersectionDistance) {
+                            closestIntersectionDistance = currentIntersectionDistance;
+                            hitSphere = &sceneSpheres[i];
+                        }
+                    }
+                }
+
+                if (hitSphere != NULL) {
+
+                    Vec3 intersectionPoint = vec3_add(primaryRay.origin, vec3_scale(primaryRay.direction, closestIntersectionDistance));
+                    Vec3 surfaceNormal = vec3_normalize(vec3_sub(intersectionPoint, hitSphere->center));
+                    Vec3 lightDirection = vec3_normalize(vec3_sub(lightPosition, intersectionPoint));
+                    double diffuseFactor = fmax(0.0, vec3_dot(surfaceNormal, lightDirection));
+
+                    pixelColor.x = hitSphere->color.x * lightColor.x * diffuseFactor + ambientLight.x;
+                    pixelColor.y = hitSphere->color.y * lightColor.y * diffuseFactor + ambientLight.y;
+                    pixelColor.z = hitSphere->color.z * lightColor.z * diffuseFactor + ambientLight.z;
+                }
+
+                Uint8 r = (Uint8)(fmax(0.0, fmin(1.0, pixelColor.x)) * 255);
+                Uint8 g = (Uint8)(fmax(0.0, fmin(1.0, pixelColor.y)) * 255);
+                Uint8 b = (Uint8)(fmax(0.0, fmin(1.0, pixelColor.z)) * 255);
+
+                SDL_SetRenderDrawColor(sdlRenderer, r, g, b, 0XFF);
+                SDL_RenderDrawPoint(sdlRenderer, x, y);
             }
         }
         SDL_RenderPresent(sdlRenderer);
