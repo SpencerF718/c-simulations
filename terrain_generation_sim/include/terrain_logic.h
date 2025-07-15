82fc18406f5e394275e4172a8fa69f4834c602ef
SpencerF718 <spencer.flaharty@gmail.com>
2025-07-15 11:58:30 -0400

    Add initial SDL setup and header
    Created the header file with the initial includes.  Added the initial SDL2 setup.

---
+++ terrain_generation_sim/include/terrain_logic.h
@@ -0,0 +1,5 @@
+#ifndef TERRAIN_LOGIC_H
+#define TERRAIN_LOGIC_H
+
+#include <SDL.h>
+
---
+++ terrain_generation_sim/src/main.c
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include "terrain_logic.h"
+
+const int WINDOW_HEIGHT = 800;
+const int WINDOW_WIDTH = 800;
+
+int main(int argc, char* argv[]) {
+
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
